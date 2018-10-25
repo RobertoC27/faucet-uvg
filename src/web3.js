f8cf9a7ffd2b8a0fbec122a3d903f8258744e952
RobertoC27 <chiroy_134@hotmail.com>
2018-10-24 19:44:35 -0600

    changed to rpc enpoint

--- src/web3.js
+++ src/web3.js
@@ -7,8 +7,8 @@ if (typeof window.web3 !== 'undefined') {
 } else {
     // If no injected web3 instance is detected, fallback to AWS instance (nodo-1).    
     
-    // var provider = new Web3.providers.HttpProvider('http://35.165.125.29:8989');
-    var provider = new Web3.providers.HttpProvider('ws://35.165.129.25:7878');
+    var provider = new Web3.providers.HttpProvider('http://35.165.125.29:8989');
+    // var provider = new Web3.providers.HttpProvider('ws://35.165.129.25:7878');
     // var provider = new Web3.providers.HttpProvider('http://127.0.0.1:9494');
     
     console.log('AWS');
